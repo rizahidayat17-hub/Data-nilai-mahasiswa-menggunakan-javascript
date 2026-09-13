570bab12e74942376e343d9b36d60a35f3eb12cb
Riza Nur Hidayat <rizanurhidayat19@gmail.com>
2026-09-13 23:32:33 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+// data.js
+export const dataMahasiswa = [
+  { id: 1, nim: "22010101", nama: "Muhammad Hafidz Lucky", nilai: 85, prodi: "Informatika" },
+  { id: 2, nim: "22010102", nama: "Siti Rahma", nilai: 78, prodi: "Informatika" },
+  { id: 3, nim: "22010103", nama: "Budi Santoso", nilai: 55, prodi: "Sistem Informasi" },
+  { id: 4, nim: "22010104", nama: "Putri Amelia Nur Sholihah", nilai: 92, prodi: "Informatika" },
+  { id: 5, nim: "22010105", nama: "Eko Prasetyo", nilai: 60, prodi: "Sistem Informasi" }
+];
